bdec2b6e7a7d2001feb768d63bcda1ecf1590be9
loveytheb <cjstjsn99@gamil.com>
2024-06-26 18:23:15 +0900

    유튜브 동영상 자동재생, 홈페이지 기본 틀 제작

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
+// tailwind.config.ts
 import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // src 폴더 내 모든 파일을 포함
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // components 폴더
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // pages 폴더
   ],
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
+
 export default config;
