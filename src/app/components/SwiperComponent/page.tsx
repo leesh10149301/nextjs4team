2c43cc900b20a15b8705851f1fd6b14d8724ec47
loveytheb <cjstjsn99@gamil.com>
2024-06-27 15:32:42 +0900

    이달의 선수 내용 추가

--- src/app/components/SwiperComponent/page.tsx
+++ src/app/components/SwiperComponent/page.tsx
@@ -39,13 +39,25 @@ const SwiperComponent: FC = () => {
             src="/images/ktwiz_schedule.png"
             className="w-[826px] mx-auto my-auto mt-20"
           />
-          <div className="w-[1099.99px] h-[680.95px] bg-gray-200 flex flex-col items-center justify-center mx-auto mt-5">
-            <div className="w-full h-[302px] bg-gray-500 flex items-center justify-center">
+          <div className="w-[1099.99px] h-[680.95px] flex flex-col items-center justify-center mx-auto mt-5">
+            <div className="w-full h-[302px] flex items-center justify-center border-2 border-red-100">
               <div className="w-[605px] h-[222px] bg-gray-100 flex items-center justify-center mx-2">
-                몇 대 몇
+                몇 대 몇<button>경기 정보</button>
               </div>
-              <div className="w-[395px] h-[222px] bg-gray-200 flex items-center justify-center mx-2">
-                이달의 선수
+              <div className="relative w-[395px] h-[222px] overflow-hidden bg-[url('/images/ktwiz_cheonplayer.png')] bg-cover">
+                <img
+                  src="/icons/ktwiz_logo.svg"
+                  className="object-cover w-[85px] h-[35px] flex items-center justify-center mt-9 ml-8 mx-2"
+                />
+                <p className="text-white text-[12px] relative bottom-2 right-12">
+                  이달의 선수
+                </p>
+                <div>
+                  <p className="text-red-600 text-5xl relative right-36">14</p>
+                  <p className="text-white text-5xl relative right-20 mr-8">
+                    천성호
+                  </p>
+                </div>
               </div>
             </div>
             <div className="w-full h-[189.98px] bg-gray-300 flex items-center justify-center mt-4">
