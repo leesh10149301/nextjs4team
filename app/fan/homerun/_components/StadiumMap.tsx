295a7d6e425f4ea14950c99b221876d0329c3773
aksenmi <aksendev@gmail.com>
2024-07-11 13:01:05 +0900

    feat: 선수별 카드목록 기본틀 생성

--- app/fan/homerun/_components/StadiumMap.tsx
+++ app/fan/homerun/_components/StadiumMap.tsx
@@ -5,25 +5,46 @@ import * as d3 from "d3";
 import hitterImage from "@/public/images/hitter.png";
 import ballImage from "@/public/images/home-run-ball.png";
 
-const StadiumMap = () => {
+interface StadiumMapProps {
+  selectedCard: number | null;
+}
+
+const StadiumMap = ({ selectedCard }: StadiumMapProps) => {
   const [isBallHit, setIsBallHit] = useState(false);
+
+  useEffect(() => {
+    if (selectedCard !== null) {
+      const hitButton = d3.select("#hit-button");
+      hitButton.classed("animate-bounce", true);
+      setTimeout(() => {
+        hitButton.classed("animate-bounce", false);
+      }, 2000);
+    }
+  }, [selectedCard]);
 
   useEffect(() => {
     const svg = d3.select("#stadium-svg");
 
     const colors: { [key: string]: string } = {
-      "중앙지정석": "#FF5733",
-      "1루 응원지정석": "#337AFF",
-      "3루 응원지정석": "#FFB533",
-      "지니TV석": "#9333FF",
-      "Y박스석": "#33FF57",
+      "중앙지정석": "#702CA4",
+      "1루 응원지정석": "#C63737",
+      "3루 응원지정석": "#C63737",
+      "지니TV석": "#509336",
+      "Y박스석": "#ED9342",
       "스카이박스": "#33D1FF",
-      "1루 스카이존": "#33FFDA",
-      "3루 스카이존": "#FFF733",
-      "티빙 테이블석(외야)": "#FF33E3",
-      "외야잔디/자유석": "#B6FF33",
-      "1루 휠체어석": "#F2C9E9",
-      "3루 휠체어석": "#F2C9E9",
+      "스카이박스 구역": "#1F2937",
+      "1루 스카이존": "#211C79",
+      "3루 스카이존": "#211C79",
+      "티빙 테이블석(외야)": "#DC7490",
+      "외야잔디/자유석1": "#BDBDBD",
+      "외야잔디/자유석2": "#BDBDBD",
+      "1루 휠체어석": "#C63737",
+      "3루 휠체어석": "#C63737",
+      "키즈랜드 캠핑존": "#1F8DCC",
+      "KT알파쇼핑석": "#CE4F9D",
+      "지니존": "#4864CB",
+      "익사이팅석": "#37A09B",
+      "하이파이브존": "#37A09B",
     };
 
     d3.json("/data/stadium_coordinates.json")
@@ -47,6 +68,8 @@ const StadiumMap = () => {
         const maxX = d3.max(allCoordinates, (d) => d[0]);
         const minY = d3.min(allCoordinates, (d) => d[1]);
         const maxY = d3.max(allCoordinates, (d) => d[1]);
+        console.log(minX, maxX);
+        console.log(minY, maxY);
 
         const padding = 300;
         const viewBox = `${minX - padding} ${minY - padding} ${
@@ -85,37 +108,98 @@ const StadiumMap = () => {
           const seatRadius = 40;
           const auraRadius = seatRadius + 10;
 
-          // 각 영역의 좌석(zone) 처리
-          area.zones.forEach((zone: any) => {
-            const { x, y } = zone.coordinates;
+          if (
+            [
+              "익사이팅석",
+              "하이파이브존",
+              "지니존",
+              "KT알파쇼핑석",
+              "키즈랜드 캠핑존",
+              "외야잔디/자유석1",
+              "외야잔디/자유석2",
+            ].includes(area.area_name)
+          ) {
+            const points = area.zones.map((zone: any) => [
+              zone.coordinates.x,
+              zone.coordinates.y,
+            ]);
+
+            const line = d3
+              .line()
+              .x((d) => d[0])
+              .y((d) => d[1])
+              .curve(d3.curveBasisClosed);
 
             areaGroup
-              .append("circle")
-              .attr("cx", x)
-              .attr("cy", y)
-              .attr("r", auraRadius)
+              .append("path")
+              .attr("id", `path-${area.area_name}`)
+              .attr("d", line(points))
               .attr("fill", colors[area.area_name] || "white")
-              .attr("opacity", 0.2);
+              .attr("opacity", 0.6)
+              .attr("stroke", colors[area.area_name] || "white")
+              .attr("stroke-width", seatRadius * 2);
 
-            areaGroup
-              .append("circle")
-              .attr("cx", x)
-              .attr("cy", y)
-              .attr("r", seatRadius)
-              .attr("fill", colors[area.area_name] || "white")
-              .attr("stroke", "black")
-              .attr("stroke-width", 2);
+            // 경로 중앙에 텍스트 추가
+            const centerX = d3.mean(points, (d) => d[0]);
+            const centerY = d3.mean(points, (d) => d[1]);
 
             areaGroup
               .append("text")
-              .attr("x", x)
-              .attr("y", y + 8)
+              .append("textPath")
+              .attr("href", `#path-${area.area_name}`) // 경로 ID 참조
+              .attr("startOffset", "20%") // 경로의 중간에 배치
+              // .attr("x", centerX)
+              // .attr("y", centerY)
               .attr("text-anchor", "middle")
-              .attr("font-size", "24px")
+              .attr("font-size", "26px")
               .attr("font-weight", "bold")
               .attr("fill", "white")
-              .text(zone.zone);
-          });
+              .text(area.area_name);
+          } else {
+            // 각 영역의 좌석(zone) 처리
+            area.zones.forEach((zone: any) => {
+              const { x, y } = zone.coordinates;
+
+              if (zone.zone === "<<< 스카이박스 >>>") {
+                areaGroup
+                  .append("text")
+                  .attr("x", x)
+                  .attr("y", y + 8)
+                  .attr("text-anchor", "middle")
+                  .attr("font-size", "26px")
+                  .attr("font-weight", "bold")
+                  .attr("fill", "white")
+                  .text(zone.zone);
+              } else {
+                areaGroup
+                  .append("circle")
+                  .attr("cx", x)
+                  .attr("cy", y)
+                  .attr("r", auraRadius)
+                  .attr("fill", colors[area.area_name] || "white")
+                  .attr("opacity", 0.2);
+
+                areaGroup
+                  .append("circle")
+                  .attr("cx", x)
+                  .attr("cy", y)
+                  .attr("r", seatRadius)
+                  .attr("fill", colors[area.area_name] || "white")
+                  .attr("stroke", "black")
+                  .attr("stroke-width", 2);
+
+                areaGroup
+                  .append("text")
+                  .attr("x", x)
+                  .attr("y", y + 8)
+                  .attr("text-anchor", "middle")
+                  .attr("font-size", "24px")
+                  .attr("font-weight", "bold")
+                  .attr("fill", "white")
+                  .text(zone.zone);
+              }
+            });
+          }
         });
 
         const facilityWidth = 160;
@@ -148,7 +232,7 @@ const StadiumMap = () => {
         });
 
         const batterX = 1820;
-        const batterY = 2950;
+        const batterY = 1800;
         const batter = svg
           .append("image")
           .attr("id", "batter-image")
@@ -235,23 +319,23 @@ const StadiumMap = () => {
       .catch((error) => {
         console.error("Error loading the JSON file:", error);
       });
-  }, [isBallHit]);
+  }, []);
 
   return (
     <div className="relative w-full h-full">
       {isBallHit ? (
         <button
           id="reset-button"
-          className="absolute top-5 left-5 z-10 bg-white p-5 rounded"
+          className="absolute  bottom-20 right-20 z-10 bg-white p-5 rounded"
         >
           공 복귀
         </button>
       ) : (
         <button
           id="hit-button"
-          className="absolute top-5 left-5 z-10 bg-white p-5 rounded"
+          className="absolute  bottom-20 right-20 z-10 bg-white p-5  rounded"
         >
-          공 치기
+          홈런볼 치기
         </button>
       )}
       <svg
--- app/fan/homerun/page.tsx
+++ app/fan/homerun/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import PlayerCard from "./_components/PlayerCard";
 import StadiumMap from "./_components/StadiumMap";
 
 export default function page() {
+  const [selectedCard, setSelectedCard] = useState<number | null>(null);
+
+  const handleCardSelect = (playerId: number) => {
+    setSelectedCard(playerId);
+  };
   return (
-    <>
-      <StadiumMap />
-    </>
+    <div className="flex flex-row p-10">
+      <StadiumMap selectedCard={selectedCard} />
+      <PlayerCard onCardSelect={handleCardSelect} />
+    </div>
   );
 }
