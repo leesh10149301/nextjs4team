e0756ff4e08e380423a9487b58c6c410100d9597
aksenmi <aksendev@gmail.com>
2024-07-18 18:08:05 +0900

    feat: 홈런볼 예측 선수 전체선택 기능 추가

--- app/fan/homerun/page.tsx
+++ app/fan/homerun/page.tsx
@@ -1,18 +1,55 @@
 "use client";
 
-import { useState } from "react";
-import PlayerCard from "./_components/PlayerCard";
-import StadiumMap from "./_components/StadiumMap";
+import React, { useEffect, useState } from "react";
+import PlayerCard from "@/app/fan/homerun/_components/PlayerCard";
+import StadiumMap from "@/app/fan/homerun/_components/StadiumMap";
 
-export default function page() {
-  const [selectedCard, setSelectedCard] = useState<number | null>(null);
+export interface PlayerHomerunData {
+  playerId: number;
+  x_coord: number;
+  y_coord: number;
+}
 
-  const handleCardSelect = (playerId: number) => {
-    setSelectedCard(playerId);
+export default function Page() {
+  const [selectedPlayerId, setSelectedPlayerId] = useState<number | null>(null);
+  const [playersHomerunData, setPlayersHomerunData] = useState<PlayerHomerunData[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleCardSelect = (playerId: number | null) => {
+    setSelectedPlayerId(playerId);
   };
+
+  const getPlayersHomerunData = async () => {
+    try {
+      const response = await fetch("/api/homerun/predict/players");
+      if (response.ok) {
+        const allPlayers = await response.json();
+        const homerunData = await Promise.all(
+          allPlayers.map(async (player: any) => {
+            const res = await fetch(`/api/homerun/predict/${player.id}`);
+            return res.json();
+          })
+        );
+        setPlayersHomerunData(homerunData);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError("Failed to fetch data");
+      setPlayersHomerunData([]);
+    }
+  };
+
+  useEffect(() => {
+    getPlayersHomerunData();
+  }, []);
+
   return (
     <div className="flex flex-row p-10">
-      <StadiumMap selectedCard={selectedCard} />
+      <StadiumMap
+        playersHomerunData={playersHomerunData}
+        selectedPlayerId={selectedPlayerId}
+        error={error}
+      />
       <PlayerCard onCardSelect={handleCardSelect} />
     </div>
   );
