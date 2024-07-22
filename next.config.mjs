8f6df74b9af7d67e42a9725580860e1fa9c5c4a0
guen9310 <guen9310@gmail.com>
2024-07-22 10:58:08 +0900

    chore : next.config.mjs 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://www.ktwiz.co.kr/api/:path*', // 여기서 프록시로 우회
-      },
-    ];
-  },
       images: {
     domains: ['www.ktwiz.co.kr','wizzap.ktwiz.co.kr'],
   },
