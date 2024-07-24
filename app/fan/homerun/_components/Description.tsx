a2cc71da167654cbc47a5ace6cad5524f7943fae
aksenmi <aksendev@gmail.com>
2024-07-24 18:04:09 +0900

    design: 선수카드 디자인 및 페이지 설명 추가

---
+++ app/fan/homerun/_components/Description.tsx
@@ -0,0 +1,9 @@
+export default function Descrption() {
+  return (
+    <>
+      <h2 className="text-center text-gray-400 w-full">
+        전체 선수 혹은 개별 선수를 선택한 후 홈런 버튼을 클릭해주세요.
+      </h2>
+    </>
+  );
+}
