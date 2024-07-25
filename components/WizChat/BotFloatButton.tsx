2e05a9272160c1bc3d9c5882fcbebe39b172cb7a
guen9310 <guen9310@gmail.com>
2024-07-25 13:01:10 +0900

    chore : 랜딩 페이지 마진 수정

--- components/WizChat/BotFloatButton.tsx
+++ components/WizChat/BotFloatButton.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function BotFloatButton({ onClick }) {
   return (
     <button
-      className="w-20 h-20 rounded-full bg-gradient-to-r from-gray-800 via-gray-900 to-black fixed bottom-4 right-4 flex items-center justify-center cursor-pointer transition-transform transform hover:scale-110 hover:shadow-lg z-50"
+      className="w-20 h-20 rounded-full bg-gradient-to-r from-gray-800 via-gray-900 to-black fixed bottom-4 right-4 flex items-center justify-center cursor-pointer transition-transform transform 
+      shadow-lg hover:scale-110 hover:shadow-lg z-50"
       onClick={onClick}
     >
       <div className="relative size-16">
